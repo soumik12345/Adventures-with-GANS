da5027b3700f8b1ccc8ebd286e746d0d6bc66188
soumik12345 <19soumik.rakshit96@gmail.com>
2019-07-28 01:33:14 +0530

    Rewrote the Discriminator Network

---
+++ CycleGAN/Discriminator.py
@@ -0,0 +1,81 @@
+from tensorflow.keras.layers import (
+    Conv2D,
+    BatchNormalization,
+    Input,
+    LeakyReLU,
+    ZeroPadding2D
+)
+from tensorflow.keras.models import Model
+
+
+class Discriminator:
+
+    def __init__(self, n_filters, max_layers, image_height, image_width, image_channels):
+        
+        self.image_height = image_height
+        self.image_width = image_width
+        self.image_channels = image_channels
+        self.n_filters = n_filters
+        self.max_layers = max_layers
+
+        self.build_model()
+    
+    def build_model(self):
+        
+        input_placeholder = Input(
+            shape = (
+                self.image_height,
+                self.image_width,
+                self.image_channels
+            ),
+            name = 'Discriminator_Input_Layer'
+        )
+        
+        x = Conv2D(
+            self.n_filters,
+            kernel_size = 4,
+            strides = 2,
+            padding = 'same',
+            name = 'First_Layer'
+        )(input_placeholder)
+        x = LeakyReLU(alpha = 0.2)(x)
+
+        for layer in range(1, self.max_layers):
+            output_features = self.n_filters * min(2 ** layer, 8)
+            x = Conv2D(
+                output_features,
+                kernel_size = 4,
+                strides = 2,
+                padding = 'same',
+                use_bias = False,
+                name = 'Branch_{0}'.format(layer)
+            )(x)
+            x = BatchNormalization()(x, training = 1)
+            x = LeakyReLU(alpha = 0.2)(x)
+        
+        output_features = self.n_filters * min(2 ** layer, 8)
+        x = ZeroPadding2D(1)(x)
+        x = Conv2D(
+            output_features,
+            kernel_size = 4,
+            use_bias = False,
+            name = 'Branch_last'
+        )(x)
+        x = BatchNormalization()(x, training = 1)
+        x = LeakyReLU(alpha = 0.2)(x)
+
+        x = ZeroPadding2D(1)(x)
+        output = Conv2D(
+            1, kernel_size = 4,
+            activation = 'sigmoid',
+            name = 'Discriminator_Output_Layer'
+        )(x)
+
+        self.discrimator = Model([input_placeholder], output)
+        print(output.get_shape)
+    
+    def display(self):
+        self.discrimator.summary()
+    
+    def save(self, model_path):
+        self.discrimator.save(model_path)
---
+++ CycleGAN/Initializers.py
@@ -0,0 +1,10 @@
+from tensorflow.keras.initializers import RandomNormal
+
+
+class Initializer:
+
+    def conv_initializer(mean = 0, stddev = 0.02):
+        return RandomNormal(mean, stddev)
+    
+    def batch_norm_initializer(mean = 1.0, stddev = 0.02):
+        return RandomNormal(mean, stddev)
