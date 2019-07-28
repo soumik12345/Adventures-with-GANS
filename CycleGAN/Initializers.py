99d97464724d4905d7861fe52da9b1682c06e046
soumik12345 <19soumik.rakshit96@gmail.com>
2019-07-29 01:21:34 +0530

    Converted Initializers to staticmethods

--- CycleGAN/Initializers.py
+++ CycleGAN/Initializers.py
@@ -3,10 +3,10 @@ from tensorflow.keras.initializers import RandomNormal
 
 class Initializer:
 
-    @classmethod
+    @staticmethod
     def conv_initializer(mean = 0, stddev = 0.02):
         return RandomNormal(mean, stddev)
     
-    @classmethod
+    @staticmethod
     def batch_norm_initializer(mean = 1.0, stddev = 0.02):
         return RandomNormal(mean, stddev)
