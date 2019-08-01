9dbdca7b3cd7ec785f91439133bf72518a4af4a2
soumik12345 <19soumik.rakshit96@gmail.com>
2019-08-01 16:34:46 +0530

    Added CycleGAN Model

--- CycleGAN/attempt_2/CycleGAN.py
+++ CycleGAN/attempt_2/CycleGAN.py
@@ -1,9 +1,11 @@
 from Generator import Generator
 from Discriminator import Discriminator
 from keras.optimizers import Adam
+from keras.layers import Input
+from keras.models import Model
 
 
-class CycleGan:
+class CycleGAN:
 
     def __init__(self, image_shape, n_filters_generators, n_filters_discriminators, lambda_cycle = 10.0):
         
@@ -13,12 +15,18 @@ class CycleGan:
         self.n_filters_discriminators = n_filters_discriminators
         self.lambda_cycle = lambda_cycle
         self.lambda_id = 0.1 * self.lambda_cycle
-
-        self.get_output_patch()
         self.optimizer = Adam(0.0002, 0.5)
 
+        self.get_output_patch()
+
         self.build_discriminators()
-        
+        self.compile_discriminators()
+
+        self.build_generators()
+
+        self.build_gan()
+
+
     
     
     def get_output_patch(self):
@@ -27,5 +35,55 @@ class CycleGan:
     
 
     def build_discriminators(self):
-        discriminator_A = Discriminator(self.image_shape, self.n_filters_discriminators)
-        discriminator_B = Discriminator(self.image_shape, self.n_filters_discriminators)
+        self.discriminator_A = Discriminator(self.image_shape, self.n_filters_discriminators).discrimator
+        self.discriminator_B = Discriminator(self.image_shape, self.n_filters_discriminators).discrimator
+    
+
+    def compile_discriminators(self):
+        self.discriminator_A.compile(loss = 'mse', optimizer = self.optimizer, metrics = ['accuracy'])
+        self.discriminator_B.compile(loss = 'mse', optimizer = self.optimizer, metrics = ['accuracy'])
+    
+
+    def build_generators(self):
+        self.generator_AB = Generator(self.image_shape, self.n_filters_generators).generator
+        self.generator_BA = Generator(self.image_shape, self.n_filters_generators).generator
+    
+
+    def build_gan(self):
+
+        self.image_A = Input(shape = self.image_shape)
+        self.image_B = Input(shape = self.image_shape)
+
+        self.fake_B = self.generator_AB(self.image_A)
+        self.fake_A = self.generator_BA(self.image_B)
+
+        self.reconstructed_A = self.generator_BA(self.fake_B)
+        self.reconstructed_B = self.generator_AB(self.fake_A)
+
+        self.image_id_A = self.generator_BA(self.reconstructed_B)
+        self.image_id_B = self.generator_AB(self.reconstructed_A)
+
+        self.discriminator_A.trainable = False
+        self.discriminator_B.trainable = False
+
+        self.valid_A = self.discriminator_A(self.fake_A)
+        self.valid_B = self.discriminator_B(self.fake_B)
+
+        self.gan = Model(
+            [self.image_A, self.image_B],
+            [
+                self.valid_A, self.valid_B,
+                self.reconstructed_A, self.reconstructed_B,
+                self.image_id_A, self.image_id_B
+            ]
+        )
+
+        self.gan.compile(
+            loss = ['mse', 'mse', 'mae', 'mae', 'mae', 'mae'],
+            loss_weights = [1, 1, self.lambda_cycle, self.lambda_cycle, self.lambda_id, self.lambda_id],
+            optimizer = self.optimizer
+        )
+    
+
+    def display(self):
+        self.gan.summary()
